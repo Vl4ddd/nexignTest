56470002d3bb0b0e534b71b0a46ca838c7116cab
unknown <ostanin8900@yandex.ru>
2025-03-24 00:30:59 +0500

    javaDoc для UdrService

--- service/src/main/java/com/nexign/service/Service/UDRService.java
+++ service/src/main/java/com/nexign/service/Service/UDRService.java
@@ -15,21 +15,50 @@ import com.nexign.service.Entity.CallDuration;
 import com.nexign.service.Entity.UDR;
 import com.nexign.service.Repository.CDRRepository;
 
+/**
+ * Service для работы с UDR.
+ * Предоставляет методы для поиска и работы с UDR.
+ * @author Влад 
+ */
 @Service
 public class UDRService {
+    
+    /** Репозиторий для работы с CDR записями */
     @Autowired
     private CDRRepository cdrRepository;
 
+    /**
+    * Получает UDR(общее время исходящих и входящих звонков) записи по номеру абонента.
+    *
+    * @param msisdn номер абонента.
+    * @param startDate дата, с которой будет произведен поиск.
+    * @param endDate дата, которой будет окончен поиск.
+    * @return обьект с номером и общей длительностью исходящих и входящих звонков.
+    */
     public UDR getUDRForSubscriber(String msisdn, LocalDateTime startDate, LocalDateTime endDate) {
         List<CDR> cdrs = cdrRepository.findByCallerNumberAndStartTimeBetween(msisdn, startDate, endDate);
         return calculateUDR(msisdn, cdrs);
     }
 
+    /**
+    * Получает UDR(общее время исходящих и входящих звонков) записи по всем абонентам.
+    *
+    * @param startDate дата, с которой будет произведен поиск.
+    * @param endDate дата, которой будет окончен поиск.
+    * @return список обьектов с номерами и общей длительностью исходящих и входящих звонков.
+    */
     public List<UDR> getUDRsForAllSubscribers(LocalDateTime startDate, LocalDateTime endDate) {
         List<CDR> cdrs = cdrRepository.findByStartTimeBetween(startDate, endDate);
         return calculateUDRs(cdrs);
     }
 
+    /**
+    * Вычисляет UDR запись для указанного абонента.
+    *
+    * @param msisdn Номер абонента, для которого требуется вычислить UDR запись.
+    * @param cdrs Список CDR записей, на основе которых будет вычислена UDR запись.
+    * @return UDR запись, содержащая информацию о входящих и исходящих звонках для указанного абонента.
+    */
     private UDR calculateUDR(String msisdn, List<CDR> cdrs) {
         long incomingDuration = 0;
         long outgoingDuration = 0;
@@ -45,6 +74,12 @@ public class UDRService {
         return new UDR(msisdn, new CallDuration(formatDuration(incomingDuration)), new CallDuration(formatDuration(outgoingDuration)));
     }
 
+    /**
+    * Вычисляет список UDR записей для всех абонентов.
+    *
+    * @param cdrs Список CDR записей, на основе которых будут вычислены UDR записи.
+    * @return Список UDR записей, содержащих информацию о входящих и исходящих звонках для всех абонентов.
+    */
     private List<UDR> calculateUDRs(List<CDR> cdrs) {
         Map<String, Long> incomingDurations = new HashMap<>();
         Map<String, Long> outgoingDurations = new HashMap<>();
@@ -79,6 +114,12 @@ public class UDRService {
         return udrList;
     }
 
+    /**
+    * Форматирует длительность в строку формата "HH:mm:ss".
+    *
+    * @param seconds Длительность в секундах, которую необходимо отформатировать.
+    * @return Строка, представляющая длительность в формате "HH:mm:ss".
+    */
     private String formatDuration(long seconds) {
         long hours = seconds / 3600;
         long minutes = (seconds % 3600) / 60;
