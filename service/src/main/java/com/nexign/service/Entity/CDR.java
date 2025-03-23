ae1708031cca257b60f8cbea1719de899c770fb5
unknown <ostanin8900@yandex.ru>
2025-03-23 18:58:59 +0500

    add CDR + sub entity

---
+++ service/src/main/java/com/nexign/service/Entity/CDR.java
@@ -0,0 +1,31 @@
+package com.nexign.service.Entity;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class CDR {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID id;
+    private String callType; 
+    private String callerNumber;
+    private String receiverNumber;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+    
+}
